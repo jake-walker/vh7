845197a81fcc2e6971d0aa630122884531e6a85c
Jake Walker <hi@jakew.me>
2021-12-28 15:28:08 +0000

    Update config

--- app/src/config.js
+++ app/src/config.js
@@ -1,7 +1,7 @@
 import { gql } from "graphql-request";
 
 const config = {
-  apiUrl: 'http://127.0.0.1:8787/graphql',
+  apiUrl: 'https://vh7.uk/graphql',
   baseUrl: 'http://vh7.uk/',
   mutations: {
     newLink: gql`
