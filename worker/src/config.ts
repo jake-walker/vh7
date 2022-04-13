e7f9cc2b2850c27f8bcec7e4e1245773f59d5840
Jake Walker <hi@jakew.me>
2022-04-13 15:33:15 +0100

    Include bots in direct user agents

--- worker/src/config.ts
+++ worker/src/config.ts
@@ -1,7 +1,9 @@
 export default {
   checkDirectUserAgent(userAgent: string | null): boolean {
     userAgent = userAgent?.toLowerCase() || "";
-    return userAgent.includes("wget") || userAgent.includes("curl");
+    return (userAgent.includes("wget") || userAgent.includes("curl") || userAgent.includes("axel")  // cli tools
+      || userAgent.includes("bot ") || userAgent.includes("bot;") || userAgent.includes("bot/")  // chat & url preview services
+      || userAgent.includes("whatsapp"));  // whatsapp & signal
   },
   frontendUrl: (VH7_ENV === 'development'
     ? 'http://localhost:3000'
