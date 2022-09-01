254e0bdfdbd59aac0bafcc7ee00e11458bbae219
Jake Walker <hi@jakew.me>
2022-09-01 20:55:28 +0100

    Fix view short link test

--- app/cypress/e2e/view.cy.js
+++ app/cypress/e2e/view.cy.js
@@ -2,9 +2,15 @@
 describe('View Page', () => {
   it('can view a short link', () => {
     cy.request('POST', 'http://localhost:8787/api/shorten', {
-      url: "https://example.com"
+      url: 'http://localhost:3000/testing123'
     }).then((res) => {
-      cy.visit(`/visit/${res.body.id}`);
+      cy.visit(`/view/${res.body.id}`);
+
+      cy.get('.mantine-Container-root > .mantine-Title-root').should('have.text', 'localhost:3000/testing123');
+
+      cy.location('href', {
+        timeout: 8000
+      }).should('eq', 'http://localhost:3000/testing123');
     });
   });
 })
