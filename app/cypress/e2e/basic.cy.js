b737aa287a5bfb8bb82e6a12d0008629fab4bf4d
Jake Walker <hi@jakew.me>
2022-09-06 22:04:16 +0100

    Finish tests

--- app/cypress/e2e/basic.cy.js
+++ app/cypress/e2e/basic.cy.js
@@ -3,32 +3,41 @@ describe('Home Page', () => {
   it('shortens', () => {
     cy.visit('/');
 
+    // fill in the form and submit
     cy.get("#shorten-url").type("https://example.com").should("have.value", "https://example.com");
     cy.get("#shorten-submit").click();
 
+    // check the success alert is shown
     cy.get("#success-alert").should("be.visible");
   });
 
   it('pastes', () => {
     cy.fixture('paste').then((code) => {
-      cy.visit('/');
+      // replace windows line endings with linux ones as it breaks the test
+      code = code.replace(/\r\n/g, '\n');
 
+      cy.visit('/');
       cy.get('#paste-tab').click();
 
+      // fill in the form
       cy.get("#paste-code").type(code).should("have.value", code);
-      cy.get("#paste-language").click()
+      // open the language drop down
+      cy.get("#paste-language").click();
+      // click on the python item in the drop down
       cy.contains("Python").click();
+      // submit the form
       cy.get("#paste-submit").click();
 
+      // check the success alert is shown
       cy.get("#success-alert").should("be.visible");
     });
   });
 
   it('uploads', () => {
     cy.visit('/');
-
     cy.get('#upload-tab').click();
 
+    // upload a file to the dropzone
     cy.get('#upload-file').get("input[type=file]").selectFile({
       contents: Cypress.Buffer.from("Hello World!"),
       fileName: "test.txt",
@@ -37,6 +46,7 @@ describe('Home Page', () => {
       force: true
     });
 
+    // check the success alert is shown
     cy.get("#success-alert").should("be.visible");
   });
 });
