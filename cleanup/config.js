a36bd037213e885f1c4189157f5e27fd4117b7a2
Jake Walker <hi@jakew.me>
2022-09-19 13:39:07 +0100

    Update cleanup script

--- cleanup/config.js
+++ cleanup/config.js
@@ -1,15 +1,22 @@
-require('dotenv').config();
+import * as dotenv from 'dotenv';
 
-module.exports = {
-  cloudflare: {
-    accountId: process.env.CF_ACCOUNT_ID,
-    authToken: process.env.CF_AUTH_TOKEN,
-    namespaceId: process.env.CF_NAMESPACE_ID
+dotenv.config({
+  path: './prod.env'
+});
+
+export default {
+  dynamodb: {
+    accessKeyId: process.env.DYNAMODB_ACCESS_KEY_ID,
+    secretAccessKey: process.env.DYNAMODB_SECRET_ACCESS_KEY,
+    endpointUrl: process.env.DYNAMODB_ENDPOINT_URL || undefined,
+    region: process.env.DYNAMODB_DEFAULT_REGION || undefined,
+    table: process.env.DYNAMODB_TABLE
   },
   s3: {
     accessKeyId: process.env.S3_ACCESS_KEY_ID,
     secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-    endpointUrl: process.env.S3_ENDPOINT_URL,
+    endpointUrl: process.env.S3_ENDPOINT_URL || undefined,
+    region: process.env.S3_DEFAULT_REGION || undefined,
     bucket: process.env.S3_BUCKET
   },
 
--- cleanup/index.js
+++ cleanup/index.js
@@ -1,89 +1,100 @@
-const WorkersKVREST = require('@sagi.io/workers-kv');
-const fs = require('fs');
-const config = require('./config');
-const { S3 } = require("aws-sdk");
-const signale = require("signale");
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import {
+  DynamoDBDocumentClient, ScanCommand, DeleteCommand
+} from '@aws-sdk/lib-dynamodb';
+import { S3Client, DeleteObjectCommand } from '@aws-sdk/client-s3';
+import signale from 'signale';
+import config from './config.js';
 
-signale.info(`Performing VH7 cleanup
- - KV Namespace: ${config.cloudflare.namespaceId.substring(0, 6)}...
- - S3 Bucket: ${config.s3.bucket}`);
-
-const s3 = new S3({
-  endpoint: config.s3.endpointUrl,
+const dbClient = new DynamoDBClient({
+  region: config.dynamodb.region,
   credentials: {
-    accessKeyId: config.s3.accessKeyId,
-    secretAccessKey: config.s3.secretAccessKey,
+    accessKeyId: config.dynamodb.accessKeyId,
+    secretAccessKey: config.dynamodb.secretAccessKey
+  },
+  endpoint: config.dynamodb.endpointUrl
+});
+
+const docClient = DynamoDBDocumentClient.from(dbClient, {
+  marshallOptions: {
+    convertEmptyValues: false,
+    removeUndefinedValues: false,
+    convertClassInstanceToMap: false,
+  },
+  unmarshallOptions: {
+    wrapNumbers: false,
   }
 });
 
-function processItem(data) {
-  // don't delete anything if newer than 2 months
-  const days = Math.floor((new Date() - new Date(data.created)) / 86400000);
-  if (days < 60) {
-    return false;
+const s3Client = new S3Client({
+  region: config.s3.region,
+  credentials: {
+    accessKeyId: config.s3.accessKeyId,
+    secretAccessKey: config.s3.secretAccessKey
+  },
+  endpoint: config.s3.endpointUrl
+});
+
+function shouldDelete(item) {
+  const now = new Date();
+  const expiry = new Date(item.expires);
+  const days = Math.floor((new Date() - new Date(item.created)) / 86400000);
+
+  // has the item expired?
+  if (item.expires !== null && expiry < now) {
+    console.debug("Reason: item has expired");
+    return true;
   }
 
-  switch (data.type) {
-    case "url:1":
-      const hostname = (new URL(data.url)).hostname;
-      // only clean up if contained in this list
-      if (config.cleanupHostnames.includes(hostname)) {
-        return true;
-      }
-      break;
-    case "upload:1":
-      // always clean up uploads
+  // is the item an upload that is at least 30 days old?
+  if (item.type.startsWith("upload:") && days > 30) {
+    console.debug("Reason: upload is older than 30 days");
+    return true;
+  }
+
+  // is the item at least 60 days old and a domain to clean up?
+  if (item.type === "url:1" && days > 60) {
+    const hostname = (new URL(item.data.url)).hostname;
+    if (config.cleanupHostnames.includes(hostname)) {
+      console.debug("Reason: cleanup domain older than 60 days");
       return true;
+    }
   }
 
   return false;
 }
 
 (async () => {
-  const WorkersKV = new WorkersKVREST({
-    cfAccountId: config.cloudflare.accountId,
-    cfAuthToken: config.cloudflare.authToken
-  });
-  const allKeys = await WorkersKV.listAllKeys({
-    namespaceId: config.cloudflare.namespaceId
-  });
-  const backup = {};
-
-  if (!allKeys.success) {
-    signale.error("Could not retrieve keys", allKeys);
-    return;
-  }
+  const documents = await docClient.send(new ScanCommand({
+    TableName: config.dynamodb.table
+  }));
 
   const status = new signale.Signale({
     interactive: true,
     scope: 'status'
   });
 
-  for (const { name: key } of allKeys.result) {
-    status.info(`Processing key ${key}...`);
+  for (const item of documents.Items) {
+    status.info(`Processing item ${item.id}...`);
 
-    const data = JSON.parse(await WorkersKV.readKey({
-      key,
-      namespaceId: config.cloudflare.namespaceId
-    }));
-    backup[key] = data;
-    const del = processItem(data);
-
-    if (del) {
-      signale.info(`Deleting key ${key}...\n${JSON.stringify(data)}`);
-      await WorkersKV.deleteKey({
-        key,
-        namespaceId: config.cloudflare.namespaceId
-      });
-      if (data.type === 'upload:1') {
-        await s3.deleteObject({
-          Bucket: config.s3.bucket,
-          Key: key
-        }).promise();
-      }
+    if (!shouldDelete(item)) {
+      continue;
     }
-  }
 
-  const d = new Date();
-  fs.writeFileSync(`./backup-${d.getDate()}-${d.getMonth()+1}-${d.getFullYear()}`, JSON.stringify(backup));
+    status.pending(`Deleting item ${item.id}...`);
+
+    if (item.type.startsWith('upload:')) {
+      await s3Client.send(new DeleteObjectCommand({
+        Bucket: config.s3.bucket,
+        Key: item.id
+      }));
+    }
+
+    await docClient.send(new DeleteCommand({
+      TableName: config.dynamodb.table,
+      Key: {
+        id: item.id
+      }
+    }));
+  }
 })();
