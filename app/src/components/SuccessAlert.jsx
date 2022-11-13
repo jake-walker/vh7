938f2359fadadfadc5eff15a67dbcee53596c4e0
Jake Walker <hi@jakew.me>
2022-11-13 10:08:34 +0000

    Generate QR codes for short links

--- app/src/components/SuccessAlert.jsx
+++ app/src/components/SuccessAlert.jsx
@@ -1,6 +1,7 @@
 import { useClipboard } from '@mantine/hooks';
-import { Alert, Text } from '@mantine/core';
+import { Alert, Text, HoverCard, Button, Group, Space } from '@mantine/core';
 import { CheckCircle } from 'react-feather';
+import { AwesomeQRCode } from "@awesomeqr/react";
 import urljoin from 'url-join';
 import { baseURL } from '../controller';
 
@@ -11,28 +12,47 @@ export function SuccessAlert({ response, clear, ...props }) {
 
   return (
     <Alert id="success-alert" icon={<CheckCircle size={32} />} title="Shortened!" color="green" withCloseButton onClose={clear} {...props}>
-      Your URL has been shortened to {' '}
-      <Text
-        id="success-alert-link"
-        variant="link"
-        component="code"
-        size="sm"
-        onClick={() => clipboard.copy(url)}
-        color={clipboard.copied ? 'green' : 'blue'}
-        sx={(theme) => ({
-          fontFamily: theme.fontFamilyMonospace,
-        })}
-      >
-        {url}
+      <Text inherit>
+        Your URL has been shortened to {' '}
+        <Text
+          inherit
+          component="code"
+          sx={(theme) => ({
+            fontFamily: theme.fontFamilyMonospace,
+            fontWeight: 'bold'
+          })}
+          >
+          {url}
+        </Text>
       </Text>
-      {' '}
-      <Text
-        component="span"
-        size="xs"
-        color="dimmed"
-      >
-        {clipboard.copied ? '(copied)' : '(click to copy)'}
-      </Text>
+      <Space h="sm" />
+      <Group spacing="xs">
+        <Button color="green" onClick={() => clipboard.copy(url)}>
+          {clipboard.copied ? 'Copied' : 'Copy'}
+        </Button>
+        <HoverCard shadow="md" withinPortal={true}>
+          <HoverCard.Target>
+            <Button variant="subtle" color="green">
+              Show QR
+            </Button>
+          </HoverCard.Target>
+          <HoverCard.Dropdown>
+            <div style={{ width: '300px', height: '300px' }}>
+              <AwesomeQRCode options={{
+                text: url,
+                autoColor: false,
+                whiteMargin: true,
+                margin: 40,
+                components: {
+                  data: { scale: 1 },
+                  timing: { scale: 1, protectors: false },
+                  alignment: { scale: 1, protectors: false },
+                  cornerAlignment: { scale: 1, protectors: false }
+                }}} />
+            </div>
+          </HoverCard.Dropdown>
+        </HoverCard>
+      </Group>
     </Alert>
   )
 }
