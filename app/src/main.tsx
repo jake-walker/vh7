0bc90ffa723be2c75fd8e7ebc568cbb37fffb5fc
Jake Walker <hi@jakew.me>
2026-02-13 19:47:44 +0000

    remove code highlighting

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,11 +1,11 @@
-import { CodeHighlightAdapterProvider, createShikiAdapter } from "@mantine/code-highlight";
+import { CodeHighlightAdapterProvider, plainTextAdapter } from "@mantine/code-highlight";
 import { MantineProvider } from "@mantine/core";
 import * as Sentry from "@sentry/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router";
-import languages from "../../languages.json";
+// import languages from "../../languages.json";
 import App from "./App.tsx";
 import { store } from "./store.ts";
 import theme from "./theme.ts";
@@ -17,16 +17,16 @@ Sentry.init({
   integrations: [],
 });
 
-async function loadShiki() {
-  const { createHighlighter } = await import("shiki/bundle/full");
-  const shiki = await createHighlighter({
-    langs: languages.map((l) => l.id),
-    themes: [],
-  });
-  return shiki;
-}
+// async function loadShiki() {
+//   const { createHighlighter } = await import("shiki/bundle/full");
+//   const shiki = await createHighlighter({
+//     langs: languages.map((l) => l.id),
+//     themes: [],
+//   });
+//   return shiki;
+// }
 
-const shikiAdapter = createShikiAdapter(loadShiki);
+// const shikiAdapter = createShikiAdapter(loadShiki);
 
 createRoot(document.getElementById("root")!, {
   onUncaughtError: Sentry.reactErrorHandler((error, errorInfo) => {
@@ -39,7 +39,7 @@ createRoot(document.getElementById("root")!, {
     <Provider store={store}>
       <BrowserRouter>
         <MantineProvider theme={theme}>
-          <CodeHighlightAdapterProvider adapter={shikiAdapter}>
+          <CodeHighlightAdapterProvider adapter={plainTextAdapter}>
             <App />
           </CodeHighlightAdapterProvider>
         </MantineProvider>
